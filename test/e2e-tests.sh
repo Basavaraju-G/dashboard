2ef0459b1090f092f84c10f33b4813b3c8c9f1e9
Basavaraju Girriamma <basavaraju013@gmail.com>
2023-04-13 21:46:30 +0530

    Update e2e-tests.sh

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -97,10 +97,10 @@ test_dashboard() {
   if $readonly; then
     CYPRESS_SPEC='-- --spec cypress/e2e/common/**/*'
   fi
-  chmod 644 ~/.kube/config
+  
   ls -l /root/.kube/
-  chmod 644 /root/.kube/config
-  docker run --rm --network=host $CYPRESS_ENV -v /root/.kube:/home/node/.kube --env KUBECONFIG=/home/node/.kube/config -v $VIDEO_PATH:/home/node/cypress/videos dashboard-e2e $CYPRESS_SPEC || fail_test "Browser E2E tests failed"
+  
+  docker run --rm --network=host $CYPRESS_ENV -v /root/.kube:/home/.kube --env KUBECONFIG=/home/.kube/config -v $VIDEO_PATH:/home/node/cypress/videos dashboard-e2e $CYPRESS_SPEC || fail_test "Browser E2E tests failed"
   # If we get here the tests passed, no need to upload artifacts
   if [ ! -z "$VIDEO_PATH" ]; then
     rm -rf $VIDEO_PATH
